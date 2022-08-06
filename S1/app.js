22ed5840908b5a6f4c81e91e469c2efa7e1d0eec
isildakmurat <yazilimkursum210110@gmail.com>
2022-08-06 14:02:25 +0300

    first codes

---
+++ S1/app.js
@@ -0,0 +1,45 @@
+// console.log('app.js is running');
+// alert('Hello World!');
+
+// // or
+
+// window.alert('Hello World!');
+
+// let firstName = prompt('Enter your name:');
+// alert('firstName:' + firstName);
+
+// let sonuc = confirm('are you legal?');
+// console.log(sonuc);
+
+
+// var myNumber1 = 11;
+// console.log(myNumber1);
+
+// let v1 = 55;
+// console.log(v1);
+
+// /*
+// some codes
+// .
+// .
+// .
+// */
+
+// var myNumber1 = 22;
+// console.log(myNumber1);
+
+// let v1 = 88;
+// console.log(v1)
+
+
+const student = {
+    name: 'Nedim',
+    age: 34,
+};
+console.log(student);
+
+const otherStudent = student;
+otherStudent.name = 'Murat';
+
+console.log(student);
+console.log(otherStudent);
