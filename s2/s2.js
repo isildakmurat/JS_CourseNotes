acb511889f96ebe0a4abe1a173cea13520b8d38b
isildakmurat <yazilimkursum210110@gmail.com>
2022-08-10 00:08:37 +0300

    new lines

---
+++ s2/s2.js
@@ -0,0 +1,34 @@
+let a = 5;
+console.log(a);
+
+{
+    let a = 3;
+    console.log(a);
+}
+
+console.log(a);
+
+//--------------
+
+var b = 5;
+console.log(b);
+
+{
+    var b = 3;
+    console.log(b);
+}
+
+console.log(b);
+
+//--------------
+
+const c = 5;
+console.log(c);
+
+{
+    const c = 3;
+    console.log(c);
+}
+
+console.log(c);
+
